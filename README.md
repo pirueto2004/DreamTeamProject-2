55cf233c60b30ef38c39c4c6c451c5bb61fff4c3
amitsinghgh19 <prasadmanigh19>
2019-06-24 23:06:37 -0400

    added home.js & index.js in models folder + login.js, inventory.js & new Products.js

--- README.md
+++ README.md
@@ -2,11 +2,11 @@
 
 A simple inventory management system for Sunglasses Warehouse using sequelize CRUD methods. Create, edit, delete products. The inventory will have the following criterias: Product ID, Product Name, Product Details(Gender, Color, Materials,Shape, Features etc), Brand, & Units.
 
-Try it [here]()!
+Try it [here]("URL")!
 
 ## Built With
 
-* CSS 5
+* CSS 4
 * HTML 5
 * JavaScript
 * [Bootstrap 4.3.0](https://getbootstrap.com/docs/4.3/getting-started/introduction/) 
---
+++ models/home.js
@@ -0,0 +1,39 @@
+module.exports = function (sequelize, DataTypes) {
+  var Procuct = sequelize.define('Procuct', {
+    // ProductId: {
+    //   type: DataTypes.INTEGER,
+    //   allowNull: false,
+    //   autoIncrement: true,
+    //   primaryKey: true
+    // },
+    ProductName: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    ProductDetails: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    Brand: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    Units: {
+      type: DataTypes.INTEGER,
+      allowNull: false
+    }
+    
+  })
+
+  Product.associate = function(models) {
+    // We're saying that a new product should belong to a brand
+    // A Product can't be created without a brand due to the foreign key constraint
+    Product.belongsTo(models.Brand, {
+      foreignKey: {
+        allowNull: false
+      }
+    });
+  };
+
+  return Procucts
+}
