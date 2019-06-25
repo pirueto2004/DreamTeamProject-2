73f19786b2d8a93bc392cef621de677ae83c5d7a
amitsinghgh19 <prasadmanigh19>
2019-06-25 00:42:51 -0400

    trying to display the products on the tables

---
+++ public/js/inventory.js
@@ -0,0 +1,47 @@
+$(document).ready(function () {
+    var todayDate = new Date()
+  
+    getProducts();
+  
+    $(document).on('click', 'button.delete', function (r) {
+        deleteProduct($(this).attr('data-value'))
+      })
+
+    function getProducts () {
+      $.get('/products', function (data) {
+        for (var i = 0; i < data.length; i++) {
+          var row = createNewRow(data[i], i)
+          $('#productBody').append(row)
+        }
+      })
+    }
+  
+    function createNewRow (product, index) {
+      var row = '<tr>' +
+        '<th scope="row">' + (index + 1) + '</th>' +
+        '<td>' + product.ProductName + '</td>' +
+        '<td>' + product.ProductDetails + '</td>' +
+        '<td>' + product.Brand + '</td>' +
+        '<td>' + product.Units + '</td>' +
+       
+        '<td>' +
+          '<button class="delete product-options animated infinite pulse" data-value="' + product.id + '">delete</button>' +
+          '<button class="edit product-options animated infinite pulse" data-value="' + product.ProductId + '">edit</button>' +
+        '</td>' +
+      '</tr>'
+  
+      return row
+    }
+  
+    function deleteProduct (id) {
+      $.ajax({
+        method: 'DELETE',
+        url: '/products/' + id
+      })
+        .then(function () {
+          $('#productBody').empty()
+          getProducts()
+        })
+    }
+  })
+  
---
+++ routes/product-api-routes.js
@@ -0,0 +1,71 @@
+// *********************************************************************************
+// api-routes.js - this file offers a set of routes for displaying and saving data to the db
+// *********************************************************************************
+
+// Dependencies
+// =============================================================
+
+// Requiring our models
+var db = require('../models')
+
+// Routes
+// =============================================================
+module.exports = function (app) {
+  // GET route for getting all of the posts
+  app.get('/products', function (req, res) {
+    // Here we add an "include" property to our options in our findAll query
+    // We set the value to an array of the models we want to include in a left outer join
+    // In this case, just db.Author
+    db.product.findAll({
+      include: [db.Products]
+    }).then(function (dbProducts) {
+      res.json(dbProducts)
+    })
+  })
+
+  // Get route for retrieving a single post
+  app.get('/products/:id', function (req, res) {
+    // Here we add an "include" property to our options in our findOne query
+    // We set the value to an array of the models we want to include in a left outer join
+    // In this case, just db.Author
+    db.Item.findOne({
+      where: {
+        id: req.params.id
+      },
+      include: [db.Products]
+    }).then(function (dbProducts) {
+      res.json(dbProducts)
+    })
+  })
+
+  // POST route for saving a new post
+  app.post('/products', function (req, res) {
+    db.Item.create(req.body).then(function (dbProducts) {
+      res.json(dbProducts)
+    })
+  })
+
+  // DELETE route for deleting posts
+  app.delete('/products/:id', function (req, res) {
+    db.Item.destroy({
+      where: {
+        id: req.params.id
+      }
+    }).then(function (dbProducts) {
+      res.json(dbProducts)
+    })
+  })
+
+  // PUT route for updating posts
+  app.put('/products', function (req, res) {
+    db.Item.update(
+      req.body,
+      {
+        where: {
+          id: req.body.id
+        }
+      }).then(function (dbProducts) {
+      res.json(dbProducts)
+    })
+  })
+}
